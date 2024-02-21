af8cb41d8d2fcb4c9c7484da5ede3f7d8f8be60f
iraklikeche <irakli.kechee@gmail.com>
2024-02-21 21:34:31 +0400

    style:complete styling of login page

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,12 @@
+{{-- @props(['name','type' => 'text','placeholder'])
+
+<input name="{{$name}}" type="{{$type}}" id="{{$name}}" /> --}}
+
+{{-- resources/views/components/input.blade.php --}}
+@props(['name', 'type' => 'text', 'placeholder'])
+
+<input name="{{ $name }}" type="{{ $type }}" id="{{ $name }}" placeholder="{{ $placeholder }}"
+        class="bg-[#f6f8fa] rounded-xl w-full py-3 px-4 
+        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+"   />
+
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<title>Task Manager</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<style>
+ .bg {
+    background-image: url('{{ asset('images/defaults/bg.png') }}');
+  }
+</style>
+
+<body style="font-family: Open Sans, sans-serif">
+   {{$slot}}
+</body>
