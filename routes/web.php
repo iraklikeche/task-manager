099181c16020509aae209857f3ec0501fe367541
iraklikeche <irakli.kechee@gmail.com>
2024-02-22 13:27:18 +0400

    refactor:remove comments

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
 Route::get('/', function() {
     if (Auth::check()) {
         return redirect('/dashboard');
