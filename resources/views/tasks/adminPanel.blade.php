eaa62046c3865789e16bc40040135736462d33c9
iraklikeche <irakli.kechee@gmail.com>
2024-02-22 23:20:03 +0400

    style:create aside

--- resources/views/tasks/adminPanel.blade.php
+++ resources/views/tasks/adminPanel.blade.php
@@ -1,4 +1,31 @@
-<form method="POST" action="/logout" class="text-xs font-semibold ml-6 text-blue-500">
-  @csrf
-  <button >Log Out</button>
-</form>
+<x-layout>
+  <section>
+    <div class="flex h-screen p-8">
+      <aside class="flex flex-col bg-[#f6f8fa] px-8 py-4 items-center gap-20 rounded-xl">
+        <div>
+          
+          <img src="{{ asset('images/defaults/avatar.png') }}"/>
+        </div>
+        <div>
+          <ul class="text-[#2f363d] flex flex-col gap-6">
+            <li class="flex gap-2 items-center"><x-icons.tasks /> My Tasks</li>
+            <li class="flex gap-2 items-center"><x-icons.dueTasks /> Due</li>
+            <li class="flex gap-2 items-center"><x-icons.profile /> Profile</li>
+            
+            
+          </ul>
+        </div>
+        <form method="POST" action="/logout" class="text-xs font-semibold text-[#2f363d] mt-auto flex">
+          @csrf
+          <x-icons.logout />
+          <button >Log Out</button>
+        </form>
+      </aside>
+      <div>
+        TABLE
+      </div>
+    </div>
+</section>
+
+
+</x-layout>
