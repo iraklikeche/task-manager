c2f943dd1d99f4fc51ef2ca42459e78a0af4815b
iraklikeche <irakli.kechee@gmail.com>
2024-02-22 10:07:59 +0400

    fix:remove comments

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,12 +1,9 @@
 <!doctype html>
 <title>Task Manager</title>
-{{-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> --}}
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 @vite('resources/css/app.css')
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-{{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
 <style>
  .bg {
     background-image: url('{{ asset('images/defaults/bg.png') }}');
