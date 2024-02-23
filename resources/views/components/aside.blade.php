add3f4a406534c549b50d1582e501319afb34b22
iraklikeche <irakli.kechee@gmail.com>
2024-02-23 22:20:56 +0400

    style:create profile-page

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -6,7 +6,9 @@
     <ul class="text-[#2f363d] flex flex-col gap-6">
       <li class="flex gap-2 items-center"><x-icons.tasks /><span class="whitespace-nowrap"> My tasks</span></li>
       <li class="flex gap-2 items-center"><x-icons.dueTasks /><span> Due</span></li>
-      <li class="flex gap-2 items-center"><x-icons.profile /> <span>Profile</span></li>
+      <a href="{{ route('dashboard.profile') }}" >
+        <li class="flex gap-2 items-center"><x-icons.profile /> <span>Profile</span></li>
+      </a>
     </ul>
   </div>
   <form method="POST" action="/logout" class="text-xs font-semibold text-[#2f363d] mt-auto flex">
---
+++ resources/views/tasks/profile.blade.php
@@ -0,0 +1,33 @@
+<x-panel-layout class="pr-64">
+  <div class="mx-auto w-96 flex flex-col gap-10">
+    <h1 class="uppercase font-semibold text-3xl text-center">Profile</h1>
+    
+    <form class="flex flex-col gap-10">
+      @csrf
+      <x-form.input type="text" placeholder="Name@redberry.ge" name="email" value="{{ old('email') }}"  />
+
+      <div class="flex flex-col gap-4">
+        <h2 class="uppercase text-[#2f363d] text-center">Change Password</h2>
+        <x-form.input type="password" placeholder="Current Password" name="currpass"   value="" />
+        <x-form.input type="password" placeholder="new Password" name="newpass" value="" />
+        <x-form.input type="password" placeholder="Retype new Password" name="renewpass" value=""  />
+      </div>
+
+      <div class="flex flex-col gap-4 m-6">
+        <h2 class="uppercase text-[#2f363d] text-center mb-6">Change Photos</h2>
+        <div class="flex items-center gap-4">
+          <img src="{{ asset('images/defaults/avatar.png') }}" class="w-24"/>
+
+          <x-form.input type="file" name="profilePhoto" placeholder="" value="{{ old('profilePhoto') }}"  />
+          <a href="#" class="uppercase">Delete</a>
+        </div>
+        <div class="flex items-center gap-4">
+          <img src="{{ asset('images/defaults/bg.png') }}" class="w-24 h-24"/>
+
+          <x-form.input type="file" name="avatar" placeholder="" value="{{ old('avatar') }}"  />
+          <a href="#" class="uppercase">Delete</a>
+        </div>      </div>
+      <button type="submit" class="bg-[#499af9] uppercase font-semibold py-4 rounded-xl mt-4 text-white">Change</button>
+    </form>
+  </div>
+</x-panel-layout>
