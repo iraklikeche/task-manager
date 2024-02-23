f9795bef7bb0e91b7c87e49af7d3673b3ea14686
iraklikeche <irakli.kechee@gmail.com>
2024-02-23 21:07:59 +0400

    refactor:extract to separate component

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,15 +1,1 @@
-<x-panel-layout class="pr-64">
-  <div class="mx-auto w-96 flex flex-col gap-10">
-    <h1 class="uppercase font-semibold text-3xl text-center">Create Task</h1>
-    
-    <form class="flex flex-col gap-10">
-      @csrf
-      <x-form.input type="text" placeholder="Task name in English" name="task" value="{{ old('task') }}"  />
-      <x-form.input type="text" placeholder="Task name in Georgian" name="taskge" value="{{ old('taskge') }}"  />
-      <x-form.textarea  type="text" placeholder="Description name in English" name="description" >{{ old('taskge') }}</x-form.textarea>
-      <x-form.textarea  type="text" placeholder="Description name in Georgian" name="descriptionge" >{{ old('taskge') }}</x-form.textarea>
-      <x-form.input type="date"  name="date" value="{{ old('date') }}" placeholder=""  />
-      <button type="submit" class="bg-[#499af9] uppercase font-semibold py-4 rounded-xl mt-4 text-white">Create Task</button>
-    </form>
-  </div>
-</x-panel-layout>
+<x-taskForm-layout name="Create Task" heading="Create Task" />
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,18 +1,6 @@
-<x-panel-layout class="pr-64">
+
+<x-taskForm-layout name="Edit Changes" heading="Edit Task">
   <a href="{{ url()->previous() }}" class="flex items-center gap-4 uppercase font-semibold">
     <x-icons.arrowBack /> <span>Back</span>
   </a>
-  <div class="mx-auto w-96 flex flex-col gap-10">
-    <h1 class="uppercase font-semibold text-3xl text-center">Edit Task</h1>
-    
-    <form class="flex flex-col gap-10">
-      @csrf
-      <x-form.input type="text" placeholder="Task name in English" name="task" value="{{ old('task') }}"  />
-      <x-form.input type="text" placeholder="Task name in Georgian" name="taskge" value="{{ old('taskge') }}"  />
-      <x-form.textarea  type="text" placeholder="Description name in English" name="description" >{{ old('taskge') }}</x-form.textarea>
-      <x-form.textarea  type="text" placeholder="Description name in Georgian" name="descriptionge" >{{ old('taskge') }}</x-form.textarea>
-      <x-form.input type="date"  name="date" value="{{ old('date') }}" placeholder=""  />
-      <button type="submit" class="bg-[#499af9] uppercase font-semibold py-4 rounded-xl mt-4 text-white">Edit Changes</button>
-    </form>
-  </div>
-</x-panel-layout>
+</x-taskForm-layout>
