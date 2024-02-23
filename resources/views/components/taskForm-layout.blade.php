489985d6f9a140361f97908586d2da7ccc451abd
iraklikeche <irakli.kechee@gmail.com>
2024-02-24 00:00:29 +0400

    refactor:define input names for en and ge properly

--- resources/views/components/taskForm-layout.blade.php
+++ resources/views/components/taskForm-layout.blade.php
@@ -5,13 +5,13 @@
        <h1 class="uppercase font-semibold text-3xl text-center">{{$heading}}</h1>
         <form class="flex flex-col gap-10">
            @csrf
-          <x-form.input type="text" placeholder="Task name in English" name="task" value="{{ old('task') }}"  />
-          <x-form.input type="text" placeholder="Task name in Georgian" name="taskge" value="{{ old('taskge') }}"  />
-          <x-form.textarea  type="text" placeholder="Description name in English" name="description" >
-            {{ old('taskge') }}
+          <x-form.input type="text" placeholder="Task name in English" name="task.en" value="{{ old('task.en') }}"  />
+          <x-form.input type="text" placeholder="Task name in Georgian" name="task.ge" value="{{ old('task.ge') }}"  />
+          <x-form.textarea  type="text" placeholder="Description name in English" name="description.en" >
+            {{ old('description.en') }}
           </x-form.textarea>
-          <x-form.textarea  type="text" placeholder="Description name in Georgian" name="descriptionge" >
-            {{ old('taskge') }}
+          <x-form.textarea  type="text" placeholder="Description name in Georgian" name="description.ge" >
+            {{ old('description.ge') }}
           </x-form.textarea>
           <x-form.input type="date"  name="date" value="{{ old('date') }}" placeholder=""  />
            <button type="submit" class="bg-[#499af9] uppercase font-semibold py-4 rounded-xl mt-4 text-white">
