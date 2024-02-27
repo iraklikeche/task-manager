9e0d7ade710d3872674b57d1327db6d872ba6c98
iraklikeche <irakli.kechee@gmail.com>
2024-02-27 14:08:56 +0400

    style:style input label floating

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,18 +1,21 @@
-@props(['name', 'type' => 'text', 'placeholder', 'value' => '', 'class' => ''])
-
+@props(['name', 'type' => 'text', 'placeholder', 'value' => '', 'class' => '', 'readonly' => false])
 @php
     $dotNotation = str_replace(['[', ']'], ['.', ''], $name);
 @endphp
 
-<div>
-        <input name="{{ $name }}" type="{{ $type }}" id="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ $value }}"
+<div class="relative mb-5">
+        <input name="{{ $name }}" type="{{ $type }}" id="{{ $name }}" value="{{ $value }}"
         @class([
-                'bg-[#f6f8fa] rounded-xl w-full py-3 px-4 placeholder:text-[#959da5] focus:outline-none',
+                'bg-[#f6f8fa] rounded-xl w-full py-4 px-4 placeholder:text-[#959da5] focus:outline-none','peer',
                 'border border-red-500 focus:border-red-500 focus:ring-red-500' => $errors->has($dotNotation),
                 'focus:border-sky-500 focus:ring-1 focus:ring-sky-500' => !$errors->has($dotNotation),
                 $class
             ])
+             {{ $readonly ? 'readonly' : '' }}
          />
+        <label for="{{$name}}" class="absolute left-2 top-4 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-gray-600  transition-all">
+                {{$placeholder}}
+        </label>
 
         <x-form.error name="{{$name}}"  />
 
--- resources/views/tasks/profile.blade.php
+++ resources/views/tasks/profile.blade.php
@@ -2,15 +2,15 @@
   <div class="mx-auto w-96 flex flex-col gap-10">
     <h1 class="uppercase font-semibold text-3xl text-center">Profile</h1>
     
-    <form class="flex flex-col gap-10" method="POST" action="{{ route('user.updatePassword') }}">
+    <form class="flex flex-col gap-10" method="POST" action="{{ route('user.updatePassword') }}" enctype="multipart/form-data">
       @csrf
-      <x-form.input type="text" placeholder="Name@redberry.ge" name="email" value="{{ old('email') }}" value="{{ auth()->user()->email }}" />
+      <x-form.input type="text" placeholder="Name@redberry.ge" name="email" value="{{ auth()->user()->email }}" class="text-[#]" readonly="true" />
 
       <div class="flex flex-col gap-4">
         <h2 class="uppercase text-[#2f363d] text-center">Change Password</h2>
         <x-form.input type="password" placeholder="Current Password" name="current_password"   value="" />
         <x-form.input type="password" placeholder="new Password" name="new_password" value="" />
-        <x-form.input type="password" placeholder="Retype new Password" name="new_password_confirmation" value=""  />
+        <x-form.input type="password" placeholder="Retype new Password" name="new_password_confirmation" value="" />
       </div>
       <div class="flex flex-col gap-4 m-6">
         <h2 class="uppercase text-[#2f363d] text-center mb-6">Change Photos</h2>
